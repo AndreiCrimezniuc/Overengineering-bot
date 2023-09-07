5177b699751252525d3ec81e453fa2d639b13609
Cavliuc Igor <cavliucigor@Cavliucs-MacBook-Pro.local>
2023-09-07 16:12:56 +0300

    add package.json & dependencies and create index.ts for first command "/start"

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("hello world")
+import TelegramBot, { Message } from 'node-telegram-bot-api';
+
+const token = "6404196367:AAF7nALkn3jmmPxhf8FtNJ3ZsBdLUBGATDQ";
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/echo (.+)/, (msg: Message, match: RegExpMatchArray | null) => {
+  const chatId = msg.chat.id;
+  const resp = match ? match[1] : '';
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', (msg: Message) => {
+  const chatId = msg.chat.id;
+  const name = msg.from?.first_name || 'Unknown';
+  const lastname = msg.from?.last_name || 'Unknown';
+  
+  bot.sendMessage(chatId, `Hello ${name} ${lastname}! How are you?`);
+});
